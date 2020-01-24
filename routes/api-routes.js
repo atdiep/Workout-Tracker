00f0dc04791dcf33bf0258634feeffd099928fe8
Arnold Diep <tungd@uw.edu>
2020-01-24 15:58:54 -0800

    getting started

---
+++ routes/api-routes.js
@@ -0,0 +1,49 @@
+const Workout = require("../models/workout.js");
+const mongojs = require("mongojs")
+
+module.exports = function (app) {
+
+    app.get("/api/workouts", function (req, res) {
+        Workout.find()
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                res.status(400).json(err);
+            });
+    });
+
+    app.put("/api/workouts/:id", (req, res) => {
+        console.log(req.body)
+        Workout.findOneAndUpdate(
+            {
+                _id: mongojs.ObjectId(req.params.id)
+            },
+            {
+                $push: {
+                    exercises:
+                    {
+                        type: req.body.type || null,
+                        name: req.body.name || null,
+                        duration: req.body.duration || null,
+                        weight: req.body.weight || null,
+                        sets: req.body.sets || null,
+                        reps: req.body.reps || null,
+                        distance: req.body.distance || null,
+                    }
+                }
+            })
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                res.status(400).json(err);
+            });
+    })
+
+    app.post("/api/workouts", (req, res) => {
+        Workout.create(req.body).then(function(dbWorkout) {
+            res.json(dbWorkout);
+        });
+    });
+}
